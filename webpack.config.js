a21f79797fcd5e44ef873e3812f82509bcbaf1f5
Benjamin Congdon <bencongdon96@gmail.com>
2019-06-06 16:32:00 -0700

    React boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./app/index.js",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
