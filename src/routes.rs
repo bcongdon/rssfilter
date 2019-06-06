6d26a469ab9daf38647d9a3b584f0ee3954ae3ea
Benjamin Congdon <bencongdon96@gmail.com>
2019-06-06 14:51:07 -0700

    Custom responder and Regex form value

--- src/routes.rs
+++ src/routes.rs
@@ -1,36 +1,53 @@
 use regex::Regex;
+use rocket::http::hyper::header::{CacheControl, CacheDirective};
+use rocket::http::RawStr;
 use rocket::request::Form;
+use rocket::request::FromFormValue;
+
+#[derive(Debug)]
+struct FilterRegex(Regex);
+
+impl<'v> FromFormValue<'v> for FilterRegex {
+    type Error = &'v RawStr;
+
+    fn from_form_value(form_value: &'v RawStr) -> Result<FilterRegex, &'v RawStr> {
+        match Regex::new(form_value.as_str()) {
+            Ok(regex) => Ok(FilterRegex(regex)),
+            _ => Err(form_value),
+        }
+    }
+}
 
 #[derive(FromForm, Debug)]
 pub struct FeedQuery {
     url: String,
-    title_reject: Option<String>,
-    title_allow: Option<String>,
+    title_reject: Option<FilterRegex>,
+    title_allow: Option<FilterRegex>,
 }
 
 impl FeedQuery {
     fn filter(&self, item: &rss::Item) -> bool {
         let mut accepted = true;
         if let Some(title) = item.title() {
-            if let Some(reject_re) = self
-                .title_reject
-                .as_ref()
-                .and_then(|t| Regex::new(&t.to_string()).ok())
-            {
-                accepted &= !reject_re.is_match(title);
+            if let Some(reject_re) = &self.title_reject {
+                accepted &= !reject_re.0.is_match(title);
             }
 
-            if let Some(allow_re) = self
-                .title_allow
-                .as_ref()
-                .and_then(|t| Regex::new(&t.to_string()).ok())
+            if let Some(allow_re) = &self.title_allow
             {
-                accepted &= allow_re.is_match(title);
+                accepted &= allow_re.0.is_match(title);
             }
         }
 
         accepted
     }
+}
+
+#[derive(Responder)]
+#[response(status = 200, content_type = "xml")]
+pub struct FeedQueryResponder {
+    inner: String,
+    cache_control: CacheControl,
 }
 
 #[get("/")]
@@ -39,7 +56,7 @@ pub fn index() -> &'static str {
 }
 
 #[get("/feed?<feed..>")]
-pub fn get_feed(feed: Form<FeedQuery>) -> String {
+pub fn get_feed(feed: Form<FeedQuery>) -> FeedQueryResponder {
     println!("{:?}", feed);
     let mut channel = rss::Channel::from_url(&feed.url).unwrap();
     let items: Vec<rss::Item> = channel
@@ -49,5 +66,9 @@ pub fn get_feed(feed: Form<FeedQuery>) -> String {
         .cloned()
         .collect();
     channel.set_items(items);
-    channel.to_string()
+    FeedQueryResponder {
+        inner: channel.to_string(),
+        cache_control: CacheControl(vec![CacheDirective::MaxAge(3600)]),
+    }
+    // channel.to_string()
 }
