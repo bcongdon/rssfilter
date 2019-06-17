2bc9be071286cda197ce06dca167f4168f8531d1
Benjamin Congdon <bencongdon96@gmail.com>
2019-06-16 20:49:54 -0700

    Send CORS headers for all requests

--- src/cors.rs
+++ src/cors.rs
@@ -15,12 +15,10 @@ impl Fairing for CORS {
     }
 
     fn on_response(&self, request: &Request, response: &mut Response) {
-        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
-            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
-            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
-            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-        }
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
 
         if request.method() == Method::Options {
             response.set_header(ContentType::Plain);
